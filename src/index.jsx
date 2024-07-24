4ec0a8b1ab554d06245459c75f6903a2bbaf46f3
Prateeky63067 <shubhamy9451@gmail.com>
2024-07-24 23:28:19 +0530

    webpackConfig Path error solve

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,12 @@
 import { SnackbarProvider } from "notistack";
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
 import { store } from "./store";
 
-const rootEl = document.querySelector("#root");
-if (!rootEl) throw new Error("Cannot find root element with that id");
-const root = createRoot(rootEl);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <SnackbarProvider maxSnack={4}>
